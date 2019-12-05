6dbdc1c32e351923091a96ffe62a71be6a7fb2ed
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2019-12-05 15:03:24 +0000

    add lifetime

---
+++ lifetime/src/main.rs
@@ -0,0 +1,38 @@
+use std::fmt::Display;
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Accnounement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let s: &'static str = "I have a static lifetime.";
+    println!("{}", s);
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    longest_with_an_announcement("hoge", "foooo", "message");
+}
